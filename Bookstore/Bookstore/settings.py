9002919c2afcc47aabeb213fbdc66fcb2d3e1472
Vissu-A <37015667+Vissu-A@users.noreply.github.com>
2021-06-28 15:12:11 +0530

    changed mail password

--- Bookstore/Bookstore/settings.py
+++ Bookstore/Bookstore/settings.py
@@ -216,7 +216,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'djangoclient97@gmail.com'
-EMAIL_HOST_PASSWORD = 'django@0720'
+EMAIL_HOST_PASSWORD = 'django@0621'
 EMAIL_USE_TLS = True    # 587
 EMAIL_USE_SSL = False   # 465
 
